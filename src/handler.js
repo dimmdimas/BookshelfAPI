54e91e09a1ef2928b16fe7f9fb11256f85db6b59
dimmdimas <mdrdimas1@gmail.com>
2024-12-08 21:52:55 +0700

    get & post

---
+++ src/handler.js
@@ -0,0 +1,94 @@
+import { nanoid } from "nanoid";
+import { book } from "./book.js";
+
+export const addBookHandler = (req, h) => {
+    const {name, year, author, summary, publisher, pageCount, readPage, reading} = req.payload;
+
+    const id = nanoid(16);
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+    const finished = false;
+
+    if (pageCount === readPage) {
+        finished = true;
+    };
+
+    const Newbook = {
+        id, name, year, author, summary, publisher, pageCount, readPage, finished, reading, insertedAt, updatedAt
+    };
+
+    book.push(Newbook);
+
+    const isSuccess = book.filter((books) => books.id === id).length > 0;
+
+    if (!name) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. Mohon isi nama buku',
+        });
+        response.code(400);
+        return response;
+    };
+
+    if (readPage > pageCount) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+        });
+        response.code(400);
+        return response;
+    };
+
+    if (isSuccess) {
+        const response = h.response({
+            status: 'success',
+            message: 'Buku berasil disimpan',
+            data: {
+                bookId: id,
+            },
+        });
+        response.code(201);
+        return response;
+    };
+
+    const response = h.response({
+        status: 'fail',
+        massage: 'Book gagal disimpan'
+    });
+    response.code(500);
+    return response;
+
+    
+    
+};
+
+export const getAllBookHandler = () => ({
+    status: 'success',
+    data: {
+        book
+    },
+});
+
+export const getByIdBookHandler = (req, h) => {
+    const { bookId } = req.params;
+
+    const theBook = book.filter((n) => n.id === bookId)[0];
+
+    if (theBook !== undefined) {
+        return {
+            status: 'success',
+            data: {
+                theBook
+            },
+        };
+    }
+
+    const response = h.response ({
+        status: 'fail',
+        massage: 'Buku tidak ditemukan'
+    });
+    response.code(404);
+    return response;
+    
+};
+
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
+import { addBookHandler, getAllBookHandler, getByIdBookHandler } from "./handler.js";
+
 export const route = [
+    {
+        method: 'POST',
+        path: '/books',
+        handler: addBookHandler
+    },
     {
         method: 'GET',
         path: '/books',
-        handler: (req, h) => {
-            return 'Homepage'
-        }
-    }
+        handler: getAllBookHandler
+    },
+    {
+        method: 'GET',
+        path: '/books/{bookId}',
+        handler: getByIdBookHandler
+    },
 ];
